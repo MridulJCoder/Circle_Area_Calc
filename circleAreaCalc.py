e5865cc65147e9e33f47fae783993d64cd02ba84
MridulJCoder <76250460+MridulJCoder@users.noreply.github.com>
2021-01-04 21:02:14 +0000

    Add files via upload

---
+++ circleAreaCalc.py
@@ -0,0 +1,7 @@
+pi = 3.14125
+radius = int(input("What is the radius of the circle?: "))
+ans = 0.0
+
+ans = radius*radius*pi
+print("The area of your circle is ", ans)
+print("The way to work out the area is to multiply the radius by the pi(3.14125)")
